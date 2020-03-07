f6b10e41a16ff29496f0098b7ee2563112ce0d91
Arsen <arsenn077@gmail.com>
2020-03-07 14:38:30 +0300

    Initial Commit

---
+++ MyFavoritsPlases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFavoritsPlases
+//
+//  Created by Arsen Mkrtchyan on 3/7/20.
+//  Copyright © 2020 smu117. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
