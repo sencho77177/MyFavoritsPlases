cbc1dcbb9867ef78af18462b8d0fa79a3a2a20a2
Arsen <arsenn077@gmail.com>
2020-03-07 14:56:19 +0300

    the first commit

--- MyFavoritsPlases/ViewController.swift
+++ MyFavoritsPlases/ViewController.swift
@@ -18,3 +18,16 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+    
+    
+}
